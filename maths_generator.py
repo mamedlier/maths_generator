903264e86bc0b6ff772c1a8c5f7ad120e3c4d813
mamedlier <155229949+mamedlier@users.noreply.github.com>
2023-12-30 17:58:15 +0500

    Add files via upload

---
+++ maths_generator.py
@@ -0,0 +1,27 @@
+#! C:/Python/python
+
+# Добавляем необходимый модуль.
+import cgi
+import random
+
+form = cgi.FieldStorage()
+
+# Печатаем  страницу.
+print('Content-type: text/html\n\n')
+print('''<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<title>Генератор примеров</title>
+<style>
+span {font-weight: bold; font-size: 24px;}
+</style>
+</head>
+<body>
+<p>&nbsp;</p>
+<form method="POST" action="maths_generator.py">
+<p><input type="submit" value="Генерация примера"></p>
+</form>
+<p>Пример: <span>''', random.randint(1, 100), random.choice(['+', '-', '/', '**', '%', '//']), random.randint(1, 100), '''</span></p>
+</body>
+</html>''')
